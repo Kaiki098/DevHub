e381a159e55988f8e8b3589db3c8501de6ea6b0e
Kaiki Alvarenga <kaikialvarengasouza098@gmail.com>
2024-05-17 23:22:11 -0300

    day 2 solution

--- app/src/main/java/br/com/kaiki/devhub/MainActivity.kt
+++ app/src/main/java/br/com/kaiki/devhub/MainActivity.kt
@@ -4,16 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.kaiki.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,25 +44,66 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                     Column {
-                         Image(painter = painterResource(id = R.drawable.cat_selfie), contentDescription = "A zoom in cat photo")
-                         Text(text = "Kaiki Alvarenga de Souza")
-                         Text(text = "Kaiki098")
-                         Text(text = "Computer Science student at IFSULDEMINAS - PASSOS")
-                     }
+                    ProfileScreen()
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun HomeScreenPreview() {
+fun ProfileScreen() {
+    val boxHeight = remember {
+        150.dp
+    }
+
+    val imageHeight = remember {
+        boxHeight
+    }
+
     Column {
-        Image(painter = painterResource(id = R.drawable.cat_selfie), contentDescription = "A zoom in cat photo")
-        Text(text = "Kaiki Alvarenga de Souza")
-        Text(text = "Kaiki098")
-        Text(text = "Computer Science student at IFSULDEMINAS - PASSOS")
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = Color.DarkGray,
+                    shape = RoundedCornerShape(
+                        bottomStart = 20.dp,
+                        bottomEnd = 20.dp
+                    )
+                )
+                .height(boxHeight)
+        )
+    }
+
+    Column (
+        modifier = Modifier
+            .padding(top = boxHeight / 2)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.cat_selfie),
+            contentDescription = "A zoom in cat photo",
+            modifier = Modifier
+                .height(imageHeight)
+                .clip(shape = RoundedCornerShape(100))
+        )
+
+        Column (
+            modifier = Modifier.padding(vertical = 10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Kaiki Alvarenga de Souza", fontWeight = FontWeight.ExtraBold, fontSize = 20.sp)
+            Text(text = "Kaiki098", fontWeight = FontWeight.ExtraBold)
+        }
+        Text(text = "Computer Science student at IFSULDEMINAS - PASSOS", textAlign = TextAlign.Center)
     }
 }
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen()
+}
+
