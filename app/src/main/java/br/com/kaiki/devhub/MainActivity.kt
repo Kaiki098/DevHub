56f5819585e937b353b7ffb64e0be5f37a296ad3
Kaiki Alvarenga <kaikialvarengasouza098@gmail.com>
2024-05-17 21:58:34 -0300

    Day 1, adding first initial elements: photo, name, github user and bio

--- app/src/main/java/br/com/kaiki/devhub/MainActivity.kt
+++ app/src/main/java/br/com/kaiki/devhub/MainActivity.kt
@@ -4,17 +4,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import br.com.kaiki.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             DevHubTheme {
-                Text("Ola mundo")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                     Column {
+                         Image(painter = painterResource(id = R.drawable.cat_selfie), contentDescription = "A zoom in cat photo")
+                         Text(text = "Kaiki Alvarenga de Souza")
+                         Text(text = "Kaiki098")
+                         Text(text = "Computer Science student at IFSULDEMINAS - PASSOS")
+                     }
+                }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    Column {
+        Image(painter = painterResource(id = R.drawable.cat_selfie), contentDescription = "A zoom in cat photo")
+        Text(text = "Kaiki Alvarenga de Souza")
+        Text(text = "Kaiki098")
+        Text(text = "Computer Science student at IFSULDEMINAS - PASSOS")
     }
 }
