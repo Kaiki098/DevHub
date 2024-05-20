05e29427790c0e18ff861ce2b4e8e274946d8a05
Kaiki Alvarenga <kaikialvarengasouza098@gmail.com>
2024-05-20 14:32:07 -0300

    IDE configs, retrofit, gson dependencies and others dependencies update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,10 @@ android {
 }
 
 dependencies {
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     implementation(libs.coil.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/br/com/kaiki/devhub/MainActivity.kt
+++ app/src/main/java/br/com/kaiki/devhub/MainActivity.kt
@@ -54,7 +54,7 @@ fun ProfileScreen() {
     val imageHeight = remember {
         boxHeight
     }
-//teste
+
     Column {
         Box(
             modifier = Modifier
